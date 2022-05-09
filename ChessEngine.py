0656dd9e38f00376308abe160b8693ad3fabbc4a
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-09 23:29:06 +0530

    Update ChessEngine

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,5 +1,6 @@
 class GameState():
     def __init__(self):
+        # 8x8 2d board, each element has 2 characters.
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -9,9 +10,16 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
-        self.whiteToMove = True
+        self.moveFunctions = {'p': self.getPawnMoves,
+                              'R': self.getRookMoves, 'N': self.getKnightMoves, 'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
+
+        self.whitetoMove = True
         self.moveLog = []
-
-
-class Move():
-    pass
+        self.whiteKingLocation = (7, 4)
+        self.blackKingLocation = (0, 4)
+        self.checkMate = False
+        self.staleMate = False
+        self.enpassantPossible = ()
+        self.currentCastlingRight = CastleRights(True, True, True, True)
+        self.castleRightsLog = [CastleRights(
+            self.currentCastlingRight.wks, self.currentCastlingRight.wqs, self.currentCastlingRight.bks, self.currentCastlingRight.bqs)]
