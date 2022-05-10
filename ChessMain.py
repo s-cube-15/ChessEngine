b1ef6f4c550d2ad71108abec045d6003cb8e2fe2
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-10 23:04:20 +0530

    Update ChessMain.py

--- ChessMain.py
+++ ChessMain.py
@@ -19,29 +19,57 @@ def loadImages():
 def main():
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
-    p.display.set_caption("Chess Game")
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False
+    animate = False
     loadImages()
     running = True
     sqSelected = ()
     playerClicks = []
+    gameOver = False
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                col = location[0]//SQ_SIZE
-                row = location[1]//SQ_SIZE
-                if sqSelected == (row, col):
+                if not gameOver:
+                    location = p.mouse.get_pos()
+                    col = location[0]//SQ_SIZE
+                    row = location[1]//SQ_SIZE
+                    if sqSelected == (row, col):
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        sqSelected = (row, col)
+                        playerClicks.append(sqSelected)
+                    if len(playerClicks) == 2:
+                        move = ChessEngine.Move(
+                            playerClicks[0], playerClicks[1], gs.board)
+                        print(move.getChessNotation())
+                        for i in range(len(validMoves)):
+                            if move == validMoves[i]:
+                                gs.makeMove(validMoves[i])
+                                moveMade = True
+                                animate = True
+                                sqSelected = ()
+                                playerClicks = []
+                        if not moveMade:
+                            playerClicks = [sqSelected]
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    moveMade = True
+                    animate = False
+                if e.key == p.K_r:
+                    gs = ChessEngine.GameState()
+                    validMoves = gs.getValidMoves()
                     sqSelected = ()
-                    playerClicks = []
-                else:
-                    sqSelected(row, col)
-                    playerClicks.append(sqSelected)
-            if len(playerClicks) == 2:
+                    playerClicks = ()
+                    moveMade = False
+                    animate = False
 
 
         drawGameState(screen, gs)
