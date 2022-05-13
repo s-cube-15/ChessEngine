ff59845da53a4705351217350a554be9a91fac79
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-13 21:56:08 +0530

    Update ChessEngine.py

--- ChessEngine.py
+++ ChessEngine.py
@@ -23,3 +23,26 @@ class GameState():
         self.currentCastlingRight = CastleRights(True, True, True, True)
         self.castleRightsLog = [CastleRights(
             self.currentCastlingRight.wks, self.currentCastlingRight.wqs, self.currentCastlingRight.bks, self.currentCastlingRight.bqs)]
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whitetoMove = not self.whitetoMove
+
+        if move.pieceMoved == 'wK':
+            self.whiteKingLocation = (move.endRow, move.endCol)
+        elif move.pieceMoved == "bK":
+            self.blackKingLocation = (move.endRow, move.endCol)
+
+        if move.isPawnPromotion:
+            self.board[move.endRow][move.endCol] = move.pieceMoved[0] + 'Q'
+
+        if move.isEnpassantMove:
+            self.board[move.startRow][move.endCol] = '--'
+
+        if move.pieceMoved[1] == 'p' and abs(move.startRow - move.endRow) == 2:
+            self.enpassantPossible = (
+                (move.startRow + move.endRow)//2, move.startCol)
+        else:
+            self.enpassantPossible = ()
