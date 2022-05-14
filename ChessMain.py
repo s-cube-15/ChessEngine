09960b849d717b233c786d709239ed0402932159
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2022-05-14 09:38:50 +0530

    Update ChessMain.py

--- ChessMain.py
+++ ChessMain.py
@@ -1,9 +1,9 @@
-import pygame as p
 import ChessEngine
+import pygame as p
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
-SQ_SIZE = HEIGHT // DIMENSION
+SQ_SIZE = HEIGHT//DIMENSION
 MAX_FPS = 15
 IMAGES = {}
 
@@ -12,8 +12,8 @@ def loadImages():
     pieces = ['wp', 'wR', 'wN', 'wB', 'wK',
               'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load(
-            "images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(
+            p.image.load('Images/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
 
 
 def main():
@@ -71,32 +71,24 @@ def main():
                     moveMade = False
                     animate = False
 
+        if moveMade:
+            if animate:
+                animateMove(gs.moveLog[-1], screen, gs.board, clock)
+            validMoves = gs.getValidMoves()
+            moveMade = False
+            animate = False
 
-        drawGameState(screen, gs)
+        drawGameState(screen, gs, validMoves, sqSelected)
+
+        if gs.checkMate:
+            gameOver = True
+            if gs.whitetoMove:
+                drawText(screen, ' Black wins by Checkmate')
+            else:
+                drawText(screen, ' White wins by Checkmate')
+        elif gs.staleMate:
+            gameOver = True
+            drawText(screen, 'Stalemate')
+
         clock.tick(MAX_FPS)
         p.display.flip()
-
-
-def drawGameState(screen, gs):
-    drawBoard(screen)
-    drawPieces(screen, gs.board)
-
-
-def drawBoard(screen):
-    colors = [p.Color("white"), p.Color("grey")]
-    for r in range(DIMENSION):
-        for c in range(DIMENSION):
-            color = colors[((r+c) % 2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-
-
-def drawPieces(screen, board):
-    for r in range(DIMENSION):
-        for c in range(DIMENSION):
-            piece = board[r][c]
-            if piece != "--":
-                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-
-
-if __name__ == '__main__':
-    main()
