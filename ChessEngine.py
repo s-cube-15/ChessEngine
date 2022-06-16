bd5505306e2157aeba02a46a19d23d8384449f87
SUDHANSHU  SABALE <2020.sudhanshu.sabale@ves.ac.in>
2022-06-16 23:05:24 +0530

    Update ChessEngine.py

--- ChessEngine.py
+++ ChessEngine.py
@@ -294,3 +294,9 @@ class GameState():
             if not self.squareUnderAttack(r, c+1) and not self.squareUnderAttack(r, c+2):
                 moves.append(
                     Move((r, c), (r, c+2), self.board, isCastleMove=True))
+
+    def getQueensideCastleMoves(self, r, c, moves):
+        if self.board[r][c-1] == '--' and self.board[r][c-2] == '--' and self.board[r][c-3] == '--':
+            if not self.squareUnderAttack(r, c-1) and not self.squareUnderAttack(r, c-2):
+                moves.append(
+                    Move((r, c), (r, c-2), self.board, isCastleMove=True))
