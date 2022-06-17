042405c1047621474a19712e7306b72c1b530ff6
SUDHANSHU  SABALE <2020.sudhanshu.sabale@ves.ac.in>
2022-06-17 22:38:36 +0530

    Update ChessEngine.py

--- ChessEngine.py
+++ ChessEngine.py
@@ -300,3 +300,41 @@ class GameState():
             if not self.squareUnderAttack(r, c-1) and not self.squareUnderAttack(r, c-2):
                 moves.append(
                     Move((r, c), (r, c-2), self.board, isCastleMove=True))
+
+
+
+          class CastleRights():
+    def __init__(self, wks, bks, wqs, bqs):
+        self.wks = wks
+        self.bks = bks
+        self.wqs = wqs
+        self.bqs = bqs
+
+
+class Move():
+    ranksToRows = {"1": 7, "2": 6, "3": 5,
+                   "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2,
+                   "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board, enpassantPossible=False, isCastleMove=False):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+        self.isPawnPromotion = (self.pieceMoved == 'wp' and self.endRow == 0) or (
+            self.pieceMoved == 'bp' and self.endRow == 7)
+
+        self.isEnpassantMove = enpassantPossible
+        if self.isEnpassantMove:
+            self.pieceCaptured = 'wp' if self.pieceMoved == 'bp' else 'bp'
+
+        self.isCastleMove = isCastleMove
+
+        self.moveID = self.startRow * 1000 + \
+            self.startCol*100 + self.endRow*10 + self.endCol          
